f693bb999487398b2a974da7e35523d975d736be
KikoSilva1 <francisco@Administrator>
2025-01-27 21:03:07 +0000

    C03 norminette till ex03

--- C03/ex03/ft_strncat.c
+++ C03/ex03/ft_strncat.c
@@ -1,14 +1,17 @@
-char *ft_strncat(char *dest, char *src, unsigned int nb){
+char	*ft_strncat(char *dest, char *src, unsigned int nb)
+{
+	int				size_dest;
+	unsigned int	i;
 
-        int size_dest = 0;
-        while(dest[size_dest] != '\0'){
-                size_dest++;
-        }
-        unsigned int i = 0;
-        while(i < nb  && src[i] != '\0'){
-                dest[size_dest + i] = src[i];
-                i++;
-        }
-        dest[size_dest + i] = '\0';
-        return dest;
-} 
+	size_dest = 0;
+	while (dest[size_dest] != '\0')
+		size_dest++;
+	i = 0;
+	while (i < nb && src[i] != '\0')
+	{
+		dest[size_dest + i] = src[i];
+		i++;
+	}
+	dest[size_dest + i] = '\0';
+	return (dest);
+}
